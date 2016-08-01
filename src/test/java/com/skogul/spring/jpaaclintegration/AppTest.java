125b5f4336ce722451a9d68a882e8ebdb1aa88ed
Michael  Kluever <mskluev@skogul.com>
2016-07-31 22:00:25 -0400

    conext loading works

--- src/test/java/com/skogul/spring/jpaaclintegration/AppTest.java
+++ src/test/java/com/skogul/spring/jpaaclintegration/AppTest.java
@@ -1,6 +1,11 @@
 package com.skogul.spring.jpaaclintegration;
 
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
 import junit.framework.TestCase;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,8 +16,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = "classpath:testApplication-context.xml")
 public class AppTest extends TestCase {
 
+	@Inject
+	public EntityManager em;
+	
 	@Test
 	public void contextLoads() {
+		assertThat(em).isNotNull();
 	}
 
 }
